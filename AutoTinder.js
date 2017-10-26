be6e133c67c3a35dd47ddff8cc18f3127e86fc20
DavidLui <dlui93@yahoo.com>
2017-10-26 17:10:38 -0400

    commit2

--- AutoTinder.js
+++ AutoTinder.js
@@ -1,12 +1,16 @@
 //Author: David Lui, 2017
-var interval = Math.floor((Math.random() * 2000) + 2000);
-var run = setInterval(request , interval); // start setInterval as "run"
+var swipeIntervalMin = 2000;
+var swipeIntervalMax = 4000;
 var counter = 0;
 var ageThreshold = 27;
+
+var interval = Math.floor((Math.random() * swipeIntervalMin) + swipeIntervalMax - swipeIntervalMin);
+var run = setInterval(request , interval); // start setInterval as "run"
+
 function request() {
 	if (document.getElementsByClassName("profileCard__menu").length == 0 && document.getElementsByClassName("beacon CenterAlignOverlay").length == 0) {
 		//If you match click on keep swiping
-		if(document.evaluate("count(//span[text()='Keep Swiping'])", document).numberValue>0){
+		if(document.evaluate("count(//span[text()='Keep Swiping'])", document).numberValue > 0){
 			document.evaluate("//span[text()='Keep Swiping']", document).iterateNext().click();
 		}
 		//like until counter > 10 and dislike if > X
@@ -17,9 +21,8 @@ function request() {
 			document.querySelectorAll("button[aria-label=Like]")[0].click();
 			counter++;
 	    }
-	    //console.log(interval); // firebug or chrome log
-	    clearInterval(run); // stop the setInterval()
-	    interval = Math.floor((Math.random() * 2000) + 2000);
-	    run = setInterval(request, interval); // start the setInterval()
+	    clearInterval(run);
+	    interval = Math.floor((Math.random() * swipeIntervalMin) + swipeIntervalMax - swipeIntervalMin);
+	    run = setInterval(request, interval);
 	}
 }
